d2e34b712e6439b0f4a44f1a7924e996d4fcc25d
rich5k <richardanatsui@gmail.com>
2022-04-02 09:52:52 +0100

    removed unnecessary files, added tailwindcss and task component

---
+++ src/components/Task.jsx
@@ -0,0 +1,11 @@
+const Task = () => {
+    return ( 
+        <div className="Task">
+            <div className="name">Groceries</div>
+            <div className="item-num">4 items</div>
+            <div className="updated-date">2022-09-03</div>
+        </div>
+     );
+}
+ 
+export default Task;
