ff95da07a1397cf62356b71e0b43f41e9f4f7d45
rich5k <richardanatsui@gmail.com>
2022-04-02 14:28:00 +0100

    added add-task-form dynamically

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,7 +1,11 @@
 import Footer from "./Footer";
 import Task from "./Task";
-
+import {useState} from 'react';
 const Tasks = () => {
+    const [isHidden, setHidden] = useState("false");
+    const ToggleClass = () => {
+        setHidden(!isHidden); 
+    };
     return ( 
         <div className="Tasks text-center mt-8 grid grid-cols-3">
             <div></div>
@@ -11,11 +15,17 @@ const Tasks = () => {
                 <Task />
             </div>
             <div>
-                <button className="add-task text-green-600 bg-white p-4 text-xl font-bold rounded-full">
+                <button onClick={ToggleClass} className="add-task text-green-600 bg-white p-4 text-xl font-bold rounded-full">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="4">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
                     </svg>
                 </button>
+
+                <form action="" className={isHidden ? "add-form m-12 hidden" : "add-form m-12"}>
+                    <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-60 p-2.5  " placeholder="Groceries" required></input>
+                    <button type="submit" class="mt-4 text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Create</button>
+                </form>
+
             </div>
 
             <Footer />
