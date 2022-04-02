f11bb5c8cd7bd876ba4dfc507a44d6c151e2f2f3
rich5k <richardanatsui@gmail.com>
2022-04-02 10:42:44 +0100

    modified task and tasks component styles

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,9 +1,12 @@
 const Task = () => {
     return ( 
-        <div className="Task">
-            <div className="name">Groceries</div>
-            <div className="item-num">4 items</div>
-            <div className="updated-date">2022-09-03</div>
+        <div className="Task bg-white my-6 mx-8 text-center rounded-lg p-4">
+            <div className="name font-bold">Groceries</div>
+            <div className="grid grid-cols-3">
+                <div className="item-num">4 items</div>
+                <div></div>
+                <div className="updated-date">2022-09-03</div>
+            </div>
         </div>
      );
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -2,10 +2,14 @@ import Task from "./Task";
 
 const Tasks = () => {
     return ( 
-        <div className="Tasks">
-            <div className="text-3xl">Tasks</div>
-            <Task />
-            <Task />
+        <div className="Tasks text-center mt-8 grid grid-cols-3">
+            <div></div>
+            <div>
+                <div className="text-3xl text-white font-bold">Tasks</div>
+                <Task />
+                <Task />
+            </div>
+            <div></div>
         </div>
      );
 }
