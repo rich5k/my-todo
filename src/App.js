e0c0342b92120f93bf21be2dbd9f575bfe93f6f6
rich5k <richardanatsui@gmail.com>
2022-04-02 13:53:23 +0100

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 
 import Tasks from "./components/Tasks";
 import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import NewTask from "./components/NewTask";
 function App() {
   return (
     <div className="App">
-      <Tasks />
+      <Router>
+          <Routes>
+          {/* <Route path="/task/:id">
+                <Tasks />
+            </Route>  */}
+            <Route path="/" element={<Tasks />} />
+            <Route path="/new" element={<NewTask />} />    
+          </Routes>
+      </Router>
     </div>
   );
 }
