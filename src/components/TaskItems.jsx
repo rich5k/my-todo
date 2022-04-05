94d0b5a271d99be96d206bcd20f4516667f34dcb
rich5k <richardanatsui@gmail.com>
2022-04-05 16:05:55 +0100

    made up responsive

--- src/components/TaskItems.jsx
+++ src/components/TaskItems.jsx
@@ -69,7 +69,7 @@ const TaskItems = () => {
         // getData();
     }
     return ( 
-        <div className="TaskItems text-white grid grid-cols-3 mt-8">
+        <div className="TaskItems text-white grid lg:grid-cols-3 mt-8">
             <div>
                 <button onClick={goHome} className="go-back ml-10 mt-4">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={4}>
@@ -112,14 +112,14 @@ const TaskItems = () => {
                 </div>
             </div>
             <div className="text-center">
-                <button onClick={ToggleClass} className="add-task text-green-600 bg-white p-4 text-xl font-bold rounded-full">
+                <button onClick={ToggleClass} className="add-task text-green-600 bg-white p-4 text-xl font-bold rounded-full mb-8">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="4">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
                     </svg>
                 </button>
 
                 {/* add task item form */}
-                <form action="" className={isHidden ? "add-form m-12 hidden" : "add-form m-12"}>
+                <form action="" className={isHidden ? "add-form m-12 hidden" : "add-form m-12 flex flex-col justify-center items-center"}>
                     <input type="text" value={title} onChange={e=>setTitle(e.target.value)} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-60 p-2.5 mb-4 " placeholder="Groceries" required></input>
                     <input type="text" value={description} onChange={e=>setDesc(e.target.value)} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-60 p-2.5  " placeholder="To help me stock up..." required></input>
                     <button disabled={!title&&!description} type="submit" onClick={handleSubmit} class="mt-4 text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Create</button>
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -3,12 +3,12 @@ import Task from "./Task";
 import React,{useState,useEffect} from 'react';
 import PuffLoader from "react-spinners/PuffLoader";
 import {db} from "../firebase";
-import { collection, onSnapshot,addDoc, query, where, doc} from "firebase/firestore";
+import { collection, onSnapshot,addDoc} from "firebase/firestore";
 const Tasks = () => {
     
     const [name,setName]=useState('');
     const [tasks,setTasks]=useState([]);
-    const [itemNum,setItemNum]=useState(0);
+    // const [itemNum,setItemNum]=useState(0);
     const loading="true";
     const color= "#51E24A";
     //gets taskCategory and task data
@@ -52,7 +52,7 @@ const Tasks = () => {
 
    
     return ( 
-        <div className="Tasks text-center mt-8 grid grid-cols-3">
+        <div className="Tasks text-center mt-8 grid lg:grid-cols-3">
             <div></div>
             <div>
                 <div className="text-3xl text-white font-bold">Tasks</div>
@@ -78,7 +78,7 @@ const Tasks = () => {
                     </svg>
                 </button>
                 {/* section for task category addition form */}
-                <form action="" className={isHidden ? "add-form m-12 hidden" : "add-form m-12"}>
+                <form action="" className={isHidden ? "add-form m-12 hidden" : "add-form m-12 flex flex-col justify-center items-center"}>
                     <input type="text" value={name} onChange={e=>setName(e.target.value)} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-60 p-2.5  " placeholder="Groceries" required></input>
                     <button disabled={!name} type="submit" onClick={handleSubmit} class="mt-4 text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Create</button>
                 </form>
